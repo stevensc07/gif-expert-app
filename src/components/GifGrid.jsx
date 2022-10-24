34af110569199c0deb351fac0b6ee0cb9d33e18b
Stevens <stevensc@uninorte.edu.co>
2022-10-24 15:37:36 -0500

    gif epert app 90% , searh gif and display it

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import { getGif } from "../controllers/getGifs";
+import { GIfItem } from "./GIfItem";
 
 
 export const GifGrid = ({ category }) => {
+    const [images, setImages] = useState([]);
 
-    getGif(category);
-    
+    const getImages = async () => {
+        const newImages = await getGif(category);
+        console.log(newImages);
+        setImages(newImages);
+    }
+
+    useEffect(() => {
+        getImages();
+    }, [])
+
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola mundo</p>
+            <div className="card-grid">
+                {
+                    images.map((image) => (
+                      <GIfItem key={image.id}
+                      {...image}
+                      >
+
+                      </GIfItem>
+                    ))
+                }
+            </div> 
         </>
     )
 }
