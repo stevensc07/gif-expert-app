1095f4ef4b7d7974a34dd15c39262bdcd6b89da7
Stevens <stevensc@uninorte.edu.co>
2022-10-24 17:24:12 -0500

    gif epert app 100%

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,26 +1,18 @@
 import { useState } from 'react';
-import { AddCategory } from './components/AddCategory';
-import {GifGrid} from './components/GifGrid'
+import { AddCategory, GifGrid } from './components';
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['Harry Potter']);
     const onAddCategory = (newCategory) => {
-        //  categories.push(newCategory);
-        // console.log(newCategory)
         if (categories.includes(newCategory)) return; // validacion que no  sea la misma categoria el include es si incluye o si esta en el arreglo
         setCategories([newCategory, ...categories]);
     }
-
     return (
         <>
             <h1>GifExpertApp</h1>
-
             <AddCategory onNewCategory={(value) => onAddCategory(value)} />
-
-            
-                {
-
-                    categories.map((category) => (
+             {
+                 categories.map((category) => (
                         <GifGrid key={category}
                             category={category}
                         />
